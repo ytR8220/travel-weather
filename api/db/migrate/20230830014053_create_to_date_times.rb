d78e96f336a83aeac69eca328987fb13b917e1c4
yuto <yto.tamashiro@gmail.com>
2023-08-30 13:42:37 +0900

    migrate修正

--- api/db/migrate/20230830014053_create_to_date_times.rb
+++
@@ -1,9 +0,0 @@
-class CreateToDateTimes < ActiveRecord::Migration[7.0]
-  def change
-    create_table :to_date_times do |t|
-      t.datetime :date_time, null: false
-
-      t.timestamps
-    end
-  end
-end
--- api/db/migrate/20230830015154_create_weathers.rb
+++ api/db/migrate/20230830015154_create_weathers.rb
@@ -2,7 +2,7 @@ class CreateWeathers < ActiveRecord::Migration[7.0]
   def change
     create_table :weathers do |t|
       t.references :city, null: false, foreign_key: true
-      t.references :to_date_time, null: false, foreign_key: true
+      t.datetime :date_time, null: false
       t.string :weather, null: false
       t.float :temp, null: false
       t.float :temp_max, null: false
@@ -13,6 +13,6 @@ class CreateWeathers < ActiveRecord::Migration[7.0]
 
       t.timestamps
     end
-    add_index :weathers, [:city_id, :to_date_time_id], unique: true
+    add_index :weathers, [:city_id, :date_time], unique: true
   end
 end
