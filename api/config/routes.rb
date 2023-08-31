bc0fb6d5be5c94a4301834c3e437ae1a84f73301
yuto <yto.tamashiro@gmail.com>
2023-08-31 13:44:13 +0900

    バックエンド一旦完成

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :weathers, only: [:index, :show, :create, :destroy]
       get '/get_coordinates', to: 'weathers#get_coordinates'
-      get '/get_whether_data', to: 'weathers#get_whether_data'
+      get '/get_weather_data', to: 'weathers#get_weather_data'
     end
   end
 end
