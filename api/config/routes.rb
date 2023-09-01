53ee6f0ccd88d0341a7d690ab67eb129126f0b4f
yuto <yto.tamashiro@gmail.com>
2023-09-01 22:05:47 +0900

    routeの修正とテスト記述の削除

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -4,8 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :weathers, only: %i[index show create destroy]
-      get '/get_coordinates', to: 'weathers#get_coordinates'
-      get '/get_weather_data', to: 'weathers#get_weather_data'
+      post '/get_weather_data', to: 'weathers#get_weather_data'
     end
   end
 end
