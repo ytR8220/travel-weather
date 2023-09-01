8fde692cdcd9e36f8222bad9687c86ee1714520a
yuto <yto.tamashiro@gmail.com>
2023-09-01 23:36:58 +0900

    api連携完了

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -6,27 +6,60 @@ import Image from 'next/image';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
-const fetcher = async (args: [string, string]) => {
-  const [url, city] = args;
-  const res = await axios.post(url, { city });
-  return res.data;
+type data = {
+  id: number;
+  city_id: number;
+  date_time: string;
+  temp: number;
+  temp_max: number;
+  temp_min: number;
+  humidity: number;
+  description: string;
+  alert: string;
+  icon: string;
+  created_at: string;
+  updated_at: string;
+  weather: string;
 };
 
 export default function Home() {
   const [inputCity, setInputCity] = useState('');
+  const [currentCity, setCurrentCity] = useState('');
+  const [data, setData] = useState<data[] | null>(null);
   const [timeWeather, setTimeWeather] = useState([]);
   const [dayWeather, setDayWeather] = useState([]);
   const [getError, setGetError] = useState(false);
   const [disabled, setDisabled] = useState(true);
+  const [timeUpdatedAt, setTimeUpdatedAt] = useState('');
+  const [dayUpdatedAt, setDayUpdatedAt] = useState('');
 
   useEffect(() => {
-    console.log(123);
     inputCity.length > 0 ? setDisabled(false) : setDisabled(true);
   }, [inputCity]);
 
+  useEffect(() => {
+    if (data?.[0]) {
+      const date = new Date(data[0].updated_at);
+      date.setUTCDate(date.getUTCDate() + 9);
+      const hour = date.getHours();
+      const minute = date.getMinutes();
+      setTimeUpdatedAt(`${hour}時${String(minute).padStart(2, '0')}分`);
+    }
+  }, [data]);
+
+  useEffect(() => {
+    if (data?.[4]) {
+      const date = new Date(data[4].updated_at);
+      date.setUTCDate(date.getUTCDate() + 9);
+      const hour = date.getHours();
+      const minute = date.getMinutes();
+      setDayUpdatedAt(`${hour}時${String(minute).padStart(2, '0')}分`);
+    }
+  }, [data]);
+
   const today = new Date();
   const year = today.getFullYear();
-  const month = today.getMonth() + 1; // getMonth()は0ベースなので1を加える
+  const month = today.getMonth() + 1;
   const day = today.getDate();
   const formattedDate = `${year}年${month}月${day}日`;
 
@@ -43,10 +76,30 @@ export default function Home() {
       );
       setTimeWeather(res?.data.slice(0, 4));
       setDayWeather(res?.data.slice(4));
+      setData(res?.data);
     } catch (err) {
       setGetError(true);
     }
+    setCurrentCity(inputCity);
     setInputCity('');
+  };
+
+  const timeIndex = (index: number) => {
+    switch (index) {
+      case 0:
+        return '現在';
+      case 1:
+        return '3時間後';
+      case 2:
+        return '6時間後';
+      case 3:
+        return '12時間後';
+    }
+  };
+
+  const dayIndex = () => {
+    today.setDate(today.getDate() + 1);
+    return `${today.getMonth() + 1}/${today.getDate()}`;
   };
 
   return (
@@ -86,221 +139,117 @@ export default function Home() {
             </div>
             <GetBtn onClick={getWeather} disabled={disabled} />
           </div>
-          <div className={'w-7/12 min-w-fit mt-7 mx-auto'}>
-            <p className={'text-3xl font-bold text-gray-700 text-center'}>
-              <span>{formattedDate}</span>の<span>那覇市</span>の天気
-            </p>
-            <p className={'mt-7 text-xl'}>
-              警報：
-              <span className={'text-gray-800'}>特にありません。</span>
-            </p>
-            <ul className={'mt-3 flex justify-between text-xl gap-x-8'}>
-              <li>
-                気温<span className={'ml-5'}>31</span>℃
-              </li>
-              <li className={'text-red-600'}>
-                最高気温<span className={'ml-5'}>31</span>℃
-              </li>
-              <li className={'text-blue-600'}>
-                最低気温<span className={'ml-5'}>31</span>℃
-              </li>
-              <li className={'text-orange-600'}>
-                湿度<span className={'ml-5'}>31</span>%
-              </li>
+          {data?.[0] && (
+            <div className={'w-7/12 min-w-fit mt-7 mx-auto'}>
+              <p className={'text-3xl font-bold text-gray-700 text-center'}>
+                <span>{formattedDate}</span>の<span>{currentCity}</span>の天気
+              </p>
+              {data?.[0].alert && (
+                <p className={'mt-7 text-xl'}>
+                  警報：{data?.[0].alert}
+                  <span className={'text-gray-800'}></span>
+                </p>
+              )}
+              <ul className={'mt-3 flex justify-between text-xl gap-x-8'}>
+                <li>
+                  気温
+                  <span className={'ml-5'}>
+                    {data?.[0] && Math.round(data[0].temp)}
+                  </span>
+                  ℃
+                </li>
+                <li className={'text-red-600'}>
+                  最高気温
+                  <span className={'ml-5'}>
+                    {data?.[0] && Math.round(data[0].temp_max)}
+                  </span>
+                  ℃
+                </li>
+                <li className={'text-blue-600'}>
+                  最低気温
+                  <span className={'ml-5'}>
+                    {data?.[0] && Math.round(data[0].temp_min)}
+                  </span>
+                  ℃
+                </li>
+                <li className={'text-orange-600'}>
+                  湿度
+                  <span className={'ml-5'}>
+                    {data?.[0] && Math.round(data[0].humidity)}
+                  </span>
+                  %
+                </li>
+              </ul>
+            </div>
+          )}
+          <div className={'w-7/12 min-w-fit mt-14 mx-auto'}>
+            <ul className={'flex justify-between gap-4'}>
+              {timeWeather.map((weather: any, index: number) => (
+                <li key={index} className={'flex flex-col items-center'}>
+                  <p className={'text-gray-800'}>{timeIndex(index)}</p>
+                  <div
+                    className={
+                      'flex flex-col items-center rounded-lg backdrop-blur-sm py-4 px-6'
+                    }
+                  >
+                    <p>{weather.description}</p>
+                    <Image
+                      src={`https://openweathermap.org/img/wn/${weather.icon}@2x.png`}
+                      alt={'sunny'}
+                      width={100}
+                      height={100}
+                    />
+                    <p>
+                      <span className={'text-red-600'}>
+                        {Math.round(weather.temp_max)}℃
+                      </span>
+                      ／
+                      <span className={'text-blue-600'}>
+                        {Math.round(weather.temp_min)}℃
+                      </span>
+                    </p>
+                  </div>
+                </li>
+              ))}
             </ul>
+            <p className={'mt-2 text-right text-gray-600'}>
+              {timeUpdatedAt && `最終更新時間：${timeUpdatedAt}`}
+            </p>
           </div>
-          <ul
-            className={
-              'w-7/12 min-w-fit mt-14 mx-auto flex justify-between gap-4'
-            }
-          >
-            <li className={'flex flex-col items-center'}>
-              <p className={'text-gray-800'}>現在</p>
-              <div
-                className={
-                  'flex flex-col items-center rounded-lg backdrop-blur-sm py-4 px-6'
-                }
-              >
-                <p>雨のち晴れ</p>
-                <Image
-                  src={'https://openweathermap.org/img/wn/10d@2x.png'}
-                  alt={'sunny'}
-                  width={100}
-                  height={100}
-                />
-                <p>
-                  <span className={'text-red-600'}>31℃</span>／
-                  <span className={'text-blue-600'}>28℃</span>
-                </p>
-              </div>
-            </li>
-            <li className={'flex flex-col items-center'}>
-              <p className={'text-gray-800'}>3時間後</p>
-              <div
-                className={
-                  'flex flex-col items-center rounded-lg backdrop-blur-sm py-4 px-6'
-                }
-              >
-                <p>雨のち晴れ</p>
-                <Image
-                  src={'https://openweathermap.org/img/wn/10d@2x.png'}
-                  alt={'sunny'}
-                  width={100}
-                  height={100}
-                />
-                <p>
-                  <span className={'text-red-600'}>31℃</span>／
-                  <span className={'text-blue-600'}>28℃</span>
-                </p>
-              </div>
-            </li>
-            <li className={'flex flex-col items-center'}>
-              <p className={'text-gray-800'}>6時間後</p>
-              <div
-                className={
-                  'flex flex-col items-center rounded-lg backdrop-blur-sm py-4 px-6'
-                }
-              >
-                <p>雨のち晴れ</p>
-                <Image
-                  src={'https://openweathermap.org/img/wn/10d@2x.png'}
-                  alt={'sunny'}
-                  width={100}
-                  height={100}
-                />
-                <p>
-                  <span className={'text-red-600'}>31℃</span>／
-                  <span className={'text-blue-600'}>28℃</span>
-                </p>
-              </div>
-            </li>
-            <li className={'flex flex-col items-center'}>
-              <p className={'text-gray-800'}>12時間後</p>
-              <div
-                className={
-                  'flex flex-col items-center rounded-lg backdrop-blur-sm py-4 px-6'
-                }
-              >
-                <p>雨のち晴れ</p>
-                <Image
-                  src={'https://openweathermap.org/img/wn/10d@2x.png'}
-                  alt={'sunny'}
-                  width={100}
-                  height={100}
-                />
-                <p>
-                  <span className={'text-red-600'}>31℃</span>／
-                  <span className={'text-blue-600'}>28℃</span>
-                </p>
-              </div>
-            </li>
-          </ul>
-          <ul
-            className={
-              'w-7/12 min-w-fit mt-10 mx-auto flex justify-between gap-1'
-            }
-          >
-            <li className={'flex flex-col items-center'}>
-              <p className={'text-gray-800'}>9/1</p>
-              <div
-                className={
-                  'flex flex-col items-center rounded-lg backdrop-blur-sm py-4 px-4'
-                }
-              >
-                <p>雨のち晴れ</p>
-                <Image
-                  src={'https://openweathermap.org/img/wn/10d@2x.png'}
-                  alt={'sunny'}
-                  width={60}
-                  height={60}
-                />
-                <p>
-                  <span className={'text-red-600'}>31℃</span>／
-                  <span className={'text-blue-600'}>28℃</span>
-                </p>
-              </div>
-            </li>
-            <li className={'flex flex-col items-center'}>
-              <p className={'text-gray-800'}>9/2</p>
-              <div
-                className={
-                  'flex flex-col items-center rounded-lg backdrop-blur-sm py-4 px-4'
-                }
-              >
-                <p>雨のち晴れ</p>
-                <Image
-                  src={'https://openweathermap.org/img/wn/10d@2x.png'}
-                  alt={'sunny'}
-                  width={60}
-                  height={60}
-                />
-                <p>
-                  <span className={'text-red-600'}>31℃</span>／
-                  <span className={'text-blue-600'}>28℃</span>
-                </p>
-              </div>
-            </li>
-            <li className={'flex flex-col items-center'}>
-              <p className={'text-gray-800'}>9/3</p>
-              <div
-                className={
-                  'flex flex-col items-center rounded-lg backdrop-blur-sm py-4 px-4'
-                }
-              >
-                <p>雨のち晴れ</p>
-                <Image
-                  src={'https://openweathermap.org/img/wn/10d@2x.png'}
-                  alt={'sunny'}
-                  width={60}
-                  height={60}
-                />
-                <p>
-                  <span className={'text-red-600'}>31℃</span>／
-                  <span className={'text-blue-600'}>28℃</span>
-                </p>
-              </div>
-            </li>
-            <li className={'flex flex-col items-center'}>
-              <p className={'text-gray-800'}>9/4</p>
-              <div
-                className={
-                  'flex flex-col items-center rounded-lg backdrop-blur-sm py-4 px-4'
-                }
-              >
-                <p>雨のち晴れ</p>
-                <Image
-                  src={'https://openweathermap.org/img/wn/10d@2x.png'}
-                  alt={'sunny'}
-                  width={60}
-                  height={60}
-                />
-                <p>
-                  <span className={'text-red-600'}>31℃</span>／
-                  <span className={'text-blue-600'}>28℃</span>
-                </p>
-              </div>
-            </li>
-            <li className={'flex flex-col items-center'}>
-              <p className={'text-gray-800'}>9/5</p>
-              <div
-                className={
-                  'flex flex-col items-center rounded-lg backdrop-blur-sm py-4 px-4'
-                }
-              >
-                <p>雨のち晴れ</p>
-                <Image
-                  src={'https://openweathermap.org/img/wn/10d@2x.png'}
-                  alt={'sunny'}
-                  width={60}
-                  height={60}
-                />
-                <p>
-                  <span className={'text-red-600'}>31℃</span>／
-                  <span className={'text-blue-600'}>28℃</span>
-                </p>
-              </div>
-            </li>
-          </ul>
+          <div className={'w-7/12 min-w-fit mt-10 mx-auto'}>
+            <ul className={'flex justify-between gap-1'}>
+              {dayWeather.map((weather: any, index: number) => (
+                <li key={index} className={'flex flex-col items-center'}>
+                  <p className={'text-gray-800'}>{dayIndex()}</p>
+                  <div
+                    className={
+                      'flex flex-col items-center rounded-lg backdrop-blur-sm py-4 px-4'
+                    }
+                  >
+                    <p>{weather.description}</p>
+                    <Image
+                      src={`https://openweathermap.org/img/wn/${weather.icon}@2x.png`}
+                      alt={'sunny'}
+                      width={60}
+                      height={60}
+                    />
+                    <p>
+                      <span className={'text-red-600'}>
+                        {Math.round(weather.temp_max)}℃
+                      </span>
+                      ／
+                      <span className={'text-blue-600'}>
+                        {Math.round(weather.temp_min)}℃
+                      </span>
+                    </p>
+                  </div>
+                </li>
+              ))}
+            </ul>
+            <p className={'mt-2 text-right text-gray-600'}>
+              {dayUpdatedAt && `最終更新時間：${dayUpdatedAt}`}
+            </p>
+          </div>
         </div>
       </div>
     </div>
