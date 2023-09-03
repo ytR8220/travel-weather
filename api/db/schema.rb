8be4116b7e4e3b4f2741fc33ff301063bebe6b72
yuto <yto.tamashiro@gmail.com>
2023-09-03 17:12:04 +0900

    ssh接続に変更

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,44 +10,44 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_230_830_015_154) do
-  create_table 'cities', force: :cascade do |t|
-    t.string 'name', null: false
-    t.string 'lat', null: false
-    t.string 'lon', null: false
-    t.integer 'country_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['country_id'], name: 'index_cities_on_country_id'
-    t.index %w[lat lon], name: 'index_cities_on_lat_and_lon', unique: true
-    t.index ['name'], name: 'index_cities_on_name', unique: true
+ActiveRecord::Schema[7.0].define(version: 2023_08_30_015154) do
+  create_table "cities", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "lat", null: false
+    t.string "lon", null: false
+    t.bigint "country_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["country_id"], name: "index_cities_on_country_id"
+    t.index ["lat", "lon"], name: "index_cities_on_lat_and_lon", unique: true
+    t.index ["name"], name: "index_cities_on_name", unique: true
   end
 
-  create_table 'countries', force: :cascade do |t|
-    t.string 'name', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['name'], name: 'index_countries_on_name', unique: true
+  create_table "countries", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_countries_on_name", unique: true
   end
 
-  create_table 'weathers', force: :cascade do |t|
-    t.integer 'city_id', null: false
-    t.datetime 'date_time', null: false
-    t.string 'weather', null: false
-    t.float 'temp', null: false
-    t.float 'temp_max', null: false
-    t.float 'temp_min', null: false
-    t.integer 'humidity', null: false
-    t.string 'description', null: false
-    t.text 'alert'
-    t.string 'icon', null: false
-    t.string 'data_type', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index %w[city_id date_time data_type], name: 'index_weathers_on_city_id_and_date_time_and_data_type', unique: true
-    t.index ['city_id'], name: 'index_weathers_on_city_id'
+  create_table "weathers", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
+    t.bigint "city_id", null: false
+    t.datetime "date_time", null: false
+    t.string "weather", null: false
+    t.float "temp", null: false
+    t.float "temp_max", null: false
+    t.float "temp_min", null: false
+    t.integer "humidity", null: false
+    t.string "description", null: false
+    t.text "alert"
+    t.string "icon", null: false
+    t.string "data_type", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["city_id", "date_time", "data_type"], name: "index_weathers_on_city_id_and_date_time_and_data_type", unique: true
+    t.index ["city_id"], name: "index_weathers_on_city_id"
   end
 
-  add_foreign_key 'cities', 'countries'
-  add_foreign_key 'weathers', 'cities'
+  add_foreign_key "cities", "countries"
+  add_foreign_key "weathers", "cities"
 end
