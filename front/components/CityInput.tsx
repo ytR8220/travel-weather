e9d453ce2e399fd61adf4fbd2b4eae83f2715139
yuto <yto.tamashiro@gmail.com>
2023-09-04 16:33:57 +0900

    Enter押下でinputからfocusが外れるように

--- front/components/CityInput.tsx
+++ front/components/CityInput.tsx
@@ -1,3 +1,5 @@
+import { useRef } from 'react';
+
 type props = {
   name: string;
   value: string;
@@ -5,12 +7,21 @@ type props = {
 };
 
 const CityInput: React.FC<props> = ({ name, value, onChange }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      inputRef.current?.blur();
+    }
+  };
+
   return (
     <input
+      ref={inputRef}
       type='text'
       name={name}
       value={value}
       onChange={onChange}
+      onKeyDown={handleKeyDown}
       className={'w-full py-2 px-4 focus:outline-none rounded-md'}
       placeholder={'市区町村名を入力　例）新宿区'}
     />
