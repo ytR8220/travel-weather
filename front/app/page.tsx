b9e58ccdf8ff51e79ebaa568e3aec8fba0457dce
yuto <yto.tamashiro@gmail.com>
2023-09-09 00:28:39 +0900

    [clean]frontリファクタリング
    レイアウトに関する部分はlayout.tsxに移した

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -124,192 +124,171 @@ export default function Home() {
   };
 
   return (
-    <div
-      className={"bg-[url('/bg.jpg')] bg-cover bg-center bg-no-repeat"}
-      style={{ backgroundPositionY: '40%' }}
-    >
-      <div
+    <>
+      <h1
         className={
-          'p-10 min-h-screen h-full flex justify-center items-center max-md:p-5'
+          'font-serif text-5xl text-center font-bold text-gray-900 max-md:text-3xl'
         }
       >
+        Travel Weather
+      </h1>
+      <form className={'w-full'}>
         <div
           className={
-            'container xl max-w-7xl rounded-xl bg-white/40 backdrop-blur-md px-5 py-20 max-md:py-10 max-md:px-3'
+            'w-6/12 max-lg:w-full min-w-fit mx-auto mt-7 flex justify-center gap-5'
           }
         >
-          <h1
+          <div className={'w-3/4 max-md:w-full'}>
+            <CityInput
+              inputRef={inputRef}
+              name={'city'}
+              value={inputCity}
+              onChange={handleChange}
+              onKeyDown={handleKeyDown}
+              onCompositionStart={() => setIsComposing(true)}
+              onCompositionEnd={() => setIsComposing(false)}
+            />
+            <p
+              className={`${
+                getError ? 'block text-red-600' : 'hidden'
+              } ${'pt-1'} ${'max-md:text-sm'}`}
+            >
+              {errMessage && errMessage}
+            </p>
+          </div>
+          <GetBtn onClick={getWeather} disabled={disabled} />
+        </div>
+      </form>
+      <div className={'mt-7 mx-auto relative'}>
+        {isLoading && (
+          <div
             className={
-              'font-serif text-5xl text-center font-bold text-gray-900 max-md:text-3xl'
+              'w-2/3 h-full absolute top-2/4 left-2/4 transform -translate-x-2/4 -translate-y-2/4 backdrop-blur-md bg-white/30 rounded-xl z-10 max-md:w-full'
             }
           >
-            Travel Weather
-          </h1>
-          <form className={'w-full'}>
-            <div
+            <p
               className={
-                'w-6/12 max-lg:w-full min-w-fit mx-auto mt-7 flex justify-center gap-5'
+                'text-xl absolute top-2/4 left-2/4 transform -translate-x-2/4 -translate-y-2/4'
               }
             >
-              <div className={'w-3/4 max-md:w-full'}>
-                <CityInput
-                  inputRef={inputRef}
-                  name={'city'}
-                  value={inputCity}
-                  onChange={handleChange}
-                  onKeyDown={handleKeyDown}
-                  onCompositionStart={() => setIsComposing(true)}
-                  onCompositionEnd={() => setIsComposing(false)}
-                />
-                <p
-                  className={`${
-                    getError ? 'block text-red-600' : 'hidden'
-                  } ${'pt-1'} ${'max-md:text-sm'}`}
-                >
-                  {errMessage && errMessage}
-                </p>
-              </div>
-              <GetBtn onClick={getWeather} disabled={disabled} />
-            </div>
-          </form>
-          <div className={'mt-7 mx-auto relative'}>
-            {isLoading && (
-              <div
+              Now Loading...
+            </p>
+          </div>
+        )}
+        {data?.[0] && (
+          <>
+            <div className={'w-7/12 min-w-fit mx-auto'}>
+              <p
                 className={
-                  'w-2/3 h-full absolute top-2/4 left-2/4 transform -translate-x-2/4 -translate-y-2/4 backdrop-blur-md bg-white/30 rounded-xl z-10 max-md:w-full'
+                  'text-3xl font-bold text-gray-700 text-center max-md:text-lg'
                 }
               >
-                <p
-                  className={
-                    'text-xl absolute top-2/4 left-2/4 transform -translate-x-2/4 -translate-y-2/4'
-                  }
-                >
-                  Now Loading...
+                {formatToday()}
+                <span>の</span>
+                {currentCity}の天気
+              </p>
+              {data?.[0].alert && (
+                <p className={'mt-7 text-xl'}>
+                  警報：{data?.[0].alert}
+                  <span className={'text-gray-800'}></span>
                 </p>
+              )}
+              <ul
+                className={
+                  'mt-5 flex justify-between text-xl gap-x-8 max-md:gap-x-2'
+                }
+              >
+                <li
+                  className={
+                    'max-md:flex max-md:flex-col max-md:items-center max-md:text-sm'
+                  }
+                >
+                  気温
+                  <span className={'ml-5 max-md:ml-0 max-md:text-base'}>
+                    {data?.[0] && Math.round(data[0].temp)}℃
+                  </span>
+                </li>
+                <li
+                  className={
+                    'text-red-600 max-md:flex max-md:flex-col max-md:items-center max-md:text-sm'
+                  }
+                >
+                  最高気温
+                  <span className={'ml-5 max-md:ml-0 max-md:text-base'}>
+                    {data?.[0] && Math.round(data[0].temp_max)}℃
+                  </span>
+                </li>
+                <li
+                  className={
+                    'text-blue-600 max-md:flex max-md:flex-col max-md:items-center max-md:text-sm'
+                  }
+                >
+                  最低気温
+                  <span className={'ml-5 max-md:ml-0 max-md:text-base'}>
+                    {data?.[0] && Math.round(data[0].temp_min)}℃
+                  </span>
+                </li>
+                <li
+                  className={
+                    'text-orange-600 max-md:flex max-md:flex-col max-md:items-center max-md:text-sm'
+                  }
+                >
+                  湿度
+                  <span className={'ml-5 max-md:ml-0 max-md:text-base'}>
+                    {data?.[0] && Math.round(data[0].humidity)}%
+                  </span>
+                </li>
+              </ul>
+            </div>
+            <div
+              className={
+                'w-7/12 min-w-fit mt-14 mx-auto max-md:min-w-0 max-md:w-full max-md:overflow-auto'
+              }
+            >
+              <div className={'max-md:overflow-auto'}>
+                <ul className={'flex justify-between gap-4 max-md:pb-4'}>
+                  {timeWeather.map((weather: any, index: number) => (
+                    <li key={index} className={'flex flex-col items-center'}>
+                      <p className={'text-gray-800 max-md:text-sm'}>
+                        {formatTime(weather.date_time)}
+                      </p>
+                      <DisplayTimesWeather weatherData={weather} />
+                    </li>
+                  ))}
+                </ul>
               </div>
-            )}
-            {data?.[0] && (
-              <>
-                <div className={'w-7/12 min-w-fit mx-auto'}>
-                  <p
-                    className={
-                      'text-3xl font-bold text-gray-700 text-center max-md:text-lg'
-                    }
-                  >
-                    {formatToday()}
-                    <span>の</span>
-                    {currentCity}の天気
-                  </p>
-                  {data?.[0].alert && (
-                    <p className={'mt-7 text-xl'}>
-                      警報：{data?.[0].alert}
-                      <span className={'text-gray-800'}></span>
-                    </p>
-                  )}
-                  <ul
-                    className={
-                      'mt-5 flex justify-between text-xl gap-x-8 max-md:gap-x-2'
-                    }
-                  >
-                    <li
-                      className={
-                        'max-md:flex max-md:flex-col max-md:items-center max-md:text-sm'
-                      }
-                    >
-                      気温
-                      <span className={'ml-5 max-md:ml-0 max-md:text-base'}>
-                        {data?.[0] && Math.round(data[0].temp)}℃
-                      </span>
-                    </li>
-                    <li
-                      className={
-                        'text-red-600 max-md:flex max-md:flex-col max-md:items-center max-md:text-sm'
-                      }
-                    >
-                      最高気温
-                      <span className={'ml-5 max-md:ml-0 max-md:text-base'}>
-                        {data?.[0] && Math.round(data[0].temp_max)}℃
-                      </span>
-                    </li>
-                    <li
-                      className={
-                        'text-blue-600 max-md:flex max-md:flex-col max-md:items-center max-md:text-sm'
-                      }
-                    >
-                      最低気温
-                      <span className={'ml-5 max-md:ml-0 max-md:text-base'}>
-                        {data?.[0] && Math.round(data[0].temp_min)}℃
-                      </span>
-                    </li>
-                    <li
-                      className={
-                        'text-orange-600 max-md:flex max-md:flex-col max-md:items-center max-md:text-sm'
-                      }
-                    >
-                      湿度
-                      <span className={'ml-5 max-md:ml-0 max-md:text-base'}>
-                        {data?.[0] && Math.round(data[0].humidity)}%
-                      </span>
-                    </li>
-                  </ul>
-                </div>
-                <div
+              <p className={'mt-2 text-right text-gray-600 max-md:text-sm'}>
+                {timeUpdatedAt && `最終更新時間：${timeUpdatedAt}`}
+              </p>
+            </div>
+            <div
+              className={
+                'w-7/12 min-w-fit mt-14 mx-auto max-md:min-w-0 max-md:w-full max-md:overflow-auto'
+              }
+            >
+              <div className={'max-md:overflow-auto'}>
+                <ul
                   className={
-                    'w-7/12 min-w-fit mt-14 mx-auto max-md:min-w-0 max-md:w-full max-md:overflow-auto'
+                    'flex justify-between gap-1 max-md:gap-x-4 max-md:pb-4'
                   }
                 >
-                  <div className={'max-md:overflow-auto'}>
-                    <ul className={'flex justify-between gap-4 max-md:pb-4'}>
-                      {timeWeather.map((weather: any, index: number) => (
-                        <li
-                          key={index}
-                          className={'flex flex-col items-center'}
-                        >
-                          <p className={'text-gray-800 max-md:text-sm'}>
-                            {formatTime(weather.date_time)}
-                          </p>
-                          <DisplayTimesWeather weatherData={weather} />
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-                  <p className={'mt-2 text-right text-gray-600 max-md:text-sm'}>
-                    {timeUpdatedAt && `最終更新時間：${timeUpdatedAt}`}
-                  </p>
-                </div>
-                <div
-                  className={
-                    'w-7/12 min-w-fit mt-14 mx-auto max-md:min-w-0 max-md:w-full max-md:overflow-auto'
-                  }
-                >
-                  <div className={'max-md:overflow-auto'}>
-                    <ul
-                      className={
-                        'flex justify-between gap-1 max-md:gap-x-4 max-md:pb-4'
-                      }
-                    >
-                      {dayWeather.map((weather: any, index: number) => (
-                        <li
-                          key={index}
-                          className={'flex flex-col items-center'}
-                        >
-                          <p className={'text-gray-800 max-md:text-sm'}>
-                            {formatDayOfWeek(weather.date_time)}
-                          </p>
-                          <DisplayDaysWeather weatherData={weather} />
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-                  <p className={'mt-2 text-right text-gray-600 max-md:text-sm'}>
-                    {dayUpdatedAt && `最終更新時間：${dayUpdatedAt}`}
-                  </p>
-                </div>
-              </>
-            )}
-          </div>
-        </div>
+                  {dayWeather.map((weather: any, index: number) => (
+                    <li key={index} className={'flex flex-col items-center'}>
+                      <p className={'text-gray-800 max-md:text-sm'}>
+                        {formatDayOfWeek(weather.date_time)}
+                      </p>
+                      <DisplayDaysWeather weatherData={weather} />
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <p className={'mt-2 text-right text-gray-600 max-md:text-sm'}>
+                {dayUpdatedAt && `最終更新時間：${dayUpdatedAt}`}
+              </p>
+            </div>
+          </>
+        )}
       </div>
-    </div>
+    </>
   );
 }
