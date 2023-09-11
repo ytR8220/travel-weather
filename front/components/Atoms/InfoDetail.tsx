cdde6244d2881e1fdfdb4a5cd9ec86cd37ed99bd
yuto <yto.tamashiro@gmail.com>
2023-09-11 09:06:55 +0900

    [fix]タグを修正
    親コンポーネントにliタグがあったので、子コンポーネントのliタグをpタグに修正

--- front/components/Atoms/InfoDetail.tsx
+++ front/components/Atoms/InfoDetail.tsx
@@ -1,11 +1,11 @@
 const InfoDetail: React.FC<detail> = ({ color, title, content }) => {
   return (
-    <li
+    <p
       className={`${color} max-md:flex max-md:flex-col max-md:items-center max-md:text-sm`}
     >
       {title}
       <span className={'ml-5 max-md:ml-0 max-md:text-base'}>{content}</span>
-    </li>
+    </p>
   );
 };
 
